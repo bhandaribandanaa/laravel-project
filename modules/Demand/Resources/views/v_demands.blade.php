8008a1db31c763dc809a0276a60f9332e3168587
Prakriti <prakritiadhikari2@gmail.com>
2018-08-02 14:13:28 +0545

    v_demand edited

--- modules/Demand/Resources/views/v_demands.blade.php
+++ modules/Demand/Resources/views/v_demands.blade.php
@@ -31,16 +31,17 @@
           @forelse($demands as $d)
                          <tr>
                             <td>{{$i++ ?? ""}}</td>
-                            <td>{{ $d->country->name ?? "" }}</td>
-                            <td>{{ $d->job_position ?? "" }}</td>
-                            <td>{{ $d->salary ?? "" }}</td>
-                            <td>{{ $d->type ?? "" }}</td>
-                            <td>{{ $d->request_number ?? "" }}</td>
-                            <td>{{ $d->fooding ?? "" }}</td>
-                            <td>{{ $d->accomodation ?? ""}}</td>
-                            <td>{{ $d->published_date ?? ""}}</td>
+                            <td>{{ $d->country->name ? : "" }}</td>
+                            <td>{{ $d->job_position ? : "" }}</td>
+                            <td>{{ $d->salary ? :"" }}</td>
+                            <td>{{ $d->type ? :"" }}</td>
+                            <td>{{ $d->request_number ?: "" }}</td>
+                            <td>{{ $d->fooding ?: "" }}</td>
+                            <td>{{ $d->accomodation ?: ""}}</td>
+                            <td>{{ $d->published_date ?: ""}}</td>
                             <td><a href="{{ route('applicants.add',$d->id) }}" target="_blank">Apply Online</a>
                                 </td>
+                                $x ? $x : ""
                         </tr>
                          @empty
                         <tr>
