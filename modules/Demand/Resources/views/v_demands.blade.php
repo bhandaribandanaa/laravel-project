6ff754a648b67e261411ba2acfabcd0dbd44f9bf
Prakriti <prakritiadhikari2@gmail.com>
2018-08-02 14:59:41 +0545

    fixes

--- modules/Demand/Resources/views/v_demands.blade.php
+++ modules/Demand/Resources/views/v_demands.blade.php
@@ -3,33 +3,35 @@
 @section('content')
 
                    
-                        <div class="container">
+<div class="container">
                         
-                        <h2>Latest Demand</h2>
-                                    <div class="wprt-lines style-1 custom-3">
-                                        <div class="line-1"></div>
-                                        <div class="line-2"></div>
-                                    </div>
-                                    <div class="wprt-spacer" data-desktop="40" data-mobi="40" data-smobi="40"></div>
-<table id="demands" class="table table-bordered table-striped" >
-          <thead>
-            <tr>
-                 <th width="6%">S.N</th>
-                 <th width="10%">Country</th>
-                 <th width="13%">Job Positon</th>
-                 <th>Salary</th>
-                 <th>Type</th>
-                 <th width="8%">Req No.</th>
-                 <th>Fooding</th>
-                <th width="15%">Accommodation</th>
-                <th>Date</th>
-                <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {{--*/$i=1/*--}}
-          @forelse($demands as $d)
-                         <tr>
+    <h2>Latest Demand</h2>
+        <div class="wprt-lines style-1 custom-3">
+            <div class="line-1"></div>
+                <div class="line-2"></div>
+            </div>
+            <div class="wprt-spacer" data-desktop="40" data-mobi="40" data-smobi="40">
+            </div>
+            
+            <table id="demands" class="table table-bordered table-striped" >
+                <thead>
+                    <tr>
+                        <th width="6%">S.N</th>
+                        <th width="10%">Country</th>
+                        <th width="13%">Job Positon</th>
+                        <th>Salary</th>
+                        <th>Type</th>
+                        <th width="8%">Req No.</th>
+                        <th>Fooding</th>
+                        <th width="15%">Accommodation</th>
+                        <th>Date</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {{--*/$i=1/*--}}
+                    @forelse($demands as $d)
+                        <tr>
                             <td>{{$i++ }}</td>
                             <td>{{ $d->country->name ? $d->country->name : "" }}</td>
                             <td>{{ $d->job_position ? $d->job_position : "" }}</td>
@@ -39,36 +41,34 @@
                             <td>{{ $d->fooding ? $d->fooding : "" }}</td>
                             <td>{{ $d->accomodation ? $d->accomodation : ""}}</td>
                             <td>{{ $d->published_date ? $d->published_date : ""}}</td>
-                            <td><a href="{{ route('applicants.add',$d->id) }}" target="_blank">Apply Online</a>
-                                </td>
+                            <td><a href="{{ route('applicants.add',$d->id) }}" target="_blank">Apply Online</a></td>
                         </tr>
-                         @empty
-                        <tr>
-                            <td>-</td>
-                            <td>-</td>
-                            <td>-</td>
-                            <td>-</td>
-                            <td>-</td>
-                            <td>-</td>
-                            <td>-</td>
-                        </tr>
-      There are no Demand yet!</p>
+                    @empty
+                    <tr>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>There are no Demand yet!</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                    </tr>
+                @endforelse
 
-         @endforelse
+                </tbody>
+            </table>
+        </div>
 
-</tbody>
+    <script>
+    $(document).ready( function () {
+        $('#demands').DataTable({
+        "lengthMenu": [[5, 10, 25, 50, -1], [5, 10, 25, 50, "All"]],
+        "bFilter" : false,               
+         "bLengthChange": false
 
-</table>
-<script>
-$(document).ready( function () {
-    $('#demands').DataTable({
-    "lengthMenu": [[5, 10, 25, 50, -1], [5, 10, 25, 50, "All"]],
-    "bFilter" : false,               
-     "bLengthChange": false
-
-} );
-} );
-</script>
+    } );
+    } );
+    </script>
 
 </div>
 @endsection
